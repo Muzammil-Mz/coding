068a6794bef2be0195cb5f577456715821165551
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-10-29 15:57:25 +0000

    str temp

---
+++ js/07.1-String templates.js
@@ -0,0 +1,18 @@
+//trim removes empty spaces from the word
+let first =" Muxammilll "
+// console.log(first.length);
+// let x=(first.trim());
+// console.log(x.length);
+
+// //touppercase
+// let upper=first.toUpperCase()
+
+// console.log(upper);
+
+// //to lowercase
+// let lower =first.toLowerCase()
+// console.log(lower);
+
+//slice
+// let sli=first.slice(3,8)
+// console.log(sli);
