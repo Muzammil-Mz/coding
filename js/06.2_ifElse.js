204d95a5a7db54a719e16d471291e486bedb5a36
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-02 15:55:25 +0000

    02nov

--- js/06.2_ifElse.js
+++ js/06.2_ifElse.js
@@ -30,3 +30,15 @@
 // let drink= age>=5 ? "coffe" : "milk"
 // console.log(drink);
 
+// let score = 85;
+
+// if (score >= 90) {
+//     console.log("Grade: A");
+// } else if (score >= 80) {
+//     console.log("Grade: B");
+// } else if (score >= 70) {
+//     console.log("Grade: C");
+// } else {
+//     console.log("Grade: D");
+// }
+// // Output: Grade: B
--- js/18_control_fkow.js
+++ js/18_control_fkow.js
@@ -4,3 +4,36 @@
 
 // 1.conditional statmetnts
 // this allows you to execute code based on specific conditions.
+
+// let marks=2 
+// let result=marks>50 ? "yes":"false" 
+// console.log(result);
+
+// let check=age==15 ? "yes"       
+
+
+// let age=30
+// if (age>20) {
+//     console.log("ageis greaer than 20"); 
+//      if(age>22){
+//         console.log("age is greater than 21");
+        
+//      }
+//      else{"sorry"
+        
+//      }
+// }
+
+// else {
+//     console.log("age is less than 20");
+    
+// }
+
+// let marks=80
+// let result=marks>=90 ? "grade A" : marks>=70 ? "grade c "
+// console.log(result);
+
+let str = "123";
+console.log(Number(str));      // 123
+console.log(parseInt(str));    // 123
+console.log(parseFloat("10.5"))
