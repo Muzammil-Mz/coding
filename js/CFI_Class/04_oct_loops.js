4a8e46dd350bcf1d1f1eed9d501d0da54200dc53
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-04 03:07:43 +0000

    04oct

---
+++ js/CFI_Class/04_oct_loops.js
@@ -0,0 +1,36 @@
+
+
+// for (let x=1;x<=5;x++){
+//     console.log("hello",x);
+    
+// }
+
+
+// let x=1
+// for (let x;x<=5;x++){
+//     console.log("hello",x);
+    
+// }
+
+
+
+//writing a table with with for loop
+// for (let x=1 ; x<=10; x++){
+// console.log(`3x${x}=${3*x}`);
+
+// }
+
+
+//writing table till 100
+// for (let x =1 ; x<=10000; x++){
+//     console.log(`5x${x}=${5*x}`);    
+// }
+
+
+
+// reverse Number
+// for (let x=10;x>=1;x--){
+//     console.log("hello",x);
+    
+// }
+
---
+++ js/CFI_Class/4_oct_while.js
@@ -0,0 +1,14 @@
+// let i=1
+// while (i<=5) {
+//     console.log("hello",i);
+//     i++
+
+// }
+
+// let i = 1
+// while (i <= 5) {
+//     console.log(`5x${i}=${5 * i}`);
+//     i++
+
+// }
+
