ba2e7abbdd55cdc5ccc125e5b37035632900b013
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-15 18:27:44 +0000

    15

--- hello.js
+++ hello.js
@@ -79,7 +79,7 @@
 // while (x <= 10) {
 //     if (x == 5) {
 //         break
-        
+
 //     } 
 //     console.log(x);
 //     x++
@@ -91,16 +91,61 @@
 // 1.print numbers from 1 t0 10 
 // for(let x=1;x<=10;x++){
 //     console.log(x);
-    
+
 // }
 
 
-let x=1
-while(x<=10){
-    console.log(x);
-    x++
-}
+// let x=1
+// while(x<=10){
+//     console.log(x);
+//     x++
+// }
 
 // 2.print numbers from 1 to 10 and add first 10 numbers
+// let x=0
+// let num=10
+// let total=0
+// for(x;x<=num;x++){
+//     total=total+x
+// console.log(total);
+
+// }
+// console.log("final",total);
+
+
+
+// let x=1
+// let num=10
+// let total=0
+// while(x<=num){
+//     total=total+x
+//     console.log(total);
+//     x++
+// }
+// console.log("final",total);
+
+
+
+
+
 // 3.print mumbers from 1 to 10 break , continue
+// let x=1
+// for (x;x<=10;x++){
+//     if (x==5){
+//         break
+//     }
+//     console.log(x);
+
+// }
+
+
+let x = 1
+while (x <= 10) {
+    if (x == 5) {+
+        break
+    } 
+    console.log(x);
+
+
+}
 //print numbers using function simple function , return function, function expression, arrow function
