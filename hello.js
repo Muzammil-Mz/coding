f3d5b7957e58d1f487dd5ab1265a642b032ac41d
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-15 18:57:47 +0000

    15

--- hello.js
+++ hello.js
@@ -162,13 +162,86 @@
 // }
 
 
-let x = 1
-while (x <= 10) {
-    if (x == 4) {
-        x++     //this line is included inorder to restartincrement from 4 and start with 5 after 3
-        continue
-    } console.log(x);
-    x++
+// let x = 1
+// while (x <= 10) {
+//     if (x == 4) {
+//         x++     //this line is included inorder to restartincrement from 4 and start with 5 after 3
+//         continue
+//     } console.log(x);
+//     x++
 
-}
+// }
+
+
+
 // print numbers using function simple function , return function, function expression, arrow function
+
+// function numbers(){
+//     for (let x=1;x<=10;x++){
+//         console.log(x);
+
+//     }
+// }
+// numbers()
+
+
+// function numbers(x,y){
+//     return x+y
+// }
+
+// console.log(numbers(5,3))
+
+// let evenodd=function(x){
+//     return (x%2==0)
+// }
+// console.log(evenodd(4))
+
+
+// let evenodd=(x)=>{
+//     return (x%2==0)
+// }
+// console.log(evenodd(5));
+
+
+
+// let number = +"10"
+// switch (number) {
+//     case 1:
+//         console.log("its 1");
+//         break
+//     case 2:
+//         console.log("its 2");
+//         break
+//     case 3:
+//         console.log("its 3");
+//         break
+//     case 10:
+//         console.log("its 10");
+//         break
+
+//     default:
+//         console.log("sorry");
+
+// }
+
+
+
+//nested
+
+let age=21
+if(age>18){
+    if(age==19){
+        console.log("your age is 19");
+        
+    }else if (age===20){
+        console.log("age is 20");
+        
+    }else if (age>=21){
+        console.log("age is 22");
+        
+    }
+    else {
+        console.log("age is less than 18");
+        
+    }
+}
