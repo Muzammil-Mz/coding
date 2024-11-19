814d83447433cb3656b25511745c077c55086c74
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-19 03:44:43 +0000

    19

--- js/timeout.js
+++ js/timeout.js
@@ -1,7 +1,45 @@
-function delaymessage(name,time){
-    return setTimeout(() => {
-        console.log(`hey ${name,time}  `);
+//
+
+
+
+
+
+// function delaymessage(name,time){
+//     return setTimeout(() => {
+//         console.log(`hey ${name,time}  `);
         
-    }, time * 1000);
-}
-delaymessage ("suhail",4)
+//     }, time * 1000);
+// }
+// delaymessage ("suhail",4)
+
+
+// function delaytime(name,time){
+// return setTimeout(() => {
+//     console.log(`${name,time}`);
+    
+
+
+
+// }, time * 1000);
+// }
+// delaytime ("abd",2)
+
+
+// setTimeout(()=>{
+//     console.log("HELLO");
+    
+// },2000) //here 2000 is 
+
+
+
+// setTimeout(() => {
+//     console.log("heyy");
+    
+// }, 12000)
+
+// console.log("suhail");
+
+// setTimeout((age) => {
+//     console.log(`hello, ${age}`);
+    
+// }, 2000,20);
