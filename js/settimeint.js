1bd0d53638858e173784fe134faa74fe3bad035e
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-19 04:05:02 +0000

    19

---
+++ js/settimeint.js
@@ -0,0 +1,20 @@
+//set interval
+//executes code after every given secong
+
+// let timer=setInterval(()=>{
+//     console.log("helloo");
+    
+// },2000)
+//this executes code for every 2 seconds
+
+
+// let timer=setInterval(()=>{
+//     console.log("helloo");
+    
+// },1000) //code runs for every 1 second and runs until stopped 
+
+
+
+// setTimeout(() => {
+//   clearInterval(timer)  
+// }, 5000); //this stops after 5 scenonds
