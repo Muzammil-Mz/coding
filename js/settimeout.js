cd38d8a10270c8bf2468fa218228a840cb207a41
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-21 17:19:15 +0000

    21

--- js/settimeout.js
+++ js/settimeout.js
@@ -1,3 +1,12 @@
+// function hello(){
+//     return setTimeout(()=>{
+//         console.log("hey");
+        
+//     },2000)
+// }
+// hello()
+
+
 // let time=setTimeout(() => {
 //    console.log("jj");
     
@@ -8,3 +17,11 @@
      
 //  }, 2000);
  
+
+//settimeout can also be called in this way after writing complete function
+
+// function hello(){
+//     console.log("hey");
+    
+// }
+// setTimeout(hello,2000)
