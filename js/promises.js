ee766a6140fe8ee368cb776397dd5960e1b683e6
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-24 03:41:53 +0000

    24

--- js/promises.js
+++ js/promises.js
@@ -135,22 +135,85 @@
     
 // })
 
-let evenodd=(num)=>{
+
+
+
+//successs
+// let evenodd=(num)=>{
+//     return new Promise ((resolve,reject)=>{
+//       if (num%2==y){
+//         resolve ("its even")
+//       } else {
+//         reject ("its odd")
+//       }  
+//     },2000)
+// }
+
+// evenodd()
+// .then((message)=>{
+//   console.log("success",message);
+    
+// })
+// .catch((error)=>{
+// console.log("error",error.message ,error.name);
+
+// })
+
+
+let checkstock=(product)=>{
     return new Promise ((resolve,reject)=>{
-      if (num%2==y){
-        resolve ("its even")
-      } else {
-        reject ("its odd")
-      }  
-    },2000)
+setTimeout(() => {
+  if (product=="laptop"){
+    resolve("product is available yes")
+  }  else {
+    reject("product not avaialbe")
+  }
+}, 2000);
+    })
 }
 
-evenodd()
+let blackcolor=((product)=>{
+    return new Promise ((resolve,reject)=>{
+        setTimeout(() => {
+            const black=true
+            if(black){
+                resolve("yes its black")
+            }reject("sorry its not black")
+        },2000);
+    })
+})
+
+
+let config=((product)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+           const i5=true
+           if(i5){
+            resolve("yes its i5")
+           } 
+           reject("sorry no such config")
+        }, 2000);
+    })
+})
+
+
+checkstock("laptop")
 .then((message)=>{
-  console.log("success",message);
+console.log("sucess",message);
+
+
+return blackcolor ("laptop")
+})
+.then ((message)=>{
+    console.log("success",message);
+    return config ("laptop")
+})
+.then((message)=>{
+    console.log("success",message);
     
 })
+
 .catch((error)=>{
-console.log("error",error.message ,error.name);
+console.log(error,"error");
 
 })
